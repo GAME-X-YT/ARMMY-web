5a67f389668b51ce18e1b67dd058c5d6c5f349d0
unknown <your-email@example.com>
2026-08-02 22:42:51 -1100

    Fix server.ts imports

--- server/server.ts
+++ server/server.ts
@@ -6,7 +6,7 @@ import nodemailer from 'nodemailer';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
-import supportRouter from './routes/supportRoutes.js';
+import supportRouter from './routes/supportRoutes.ts';
 dotenv.config();
 
 const app = express();
